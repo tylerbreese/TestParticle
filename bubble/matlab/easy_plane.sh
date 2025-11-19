a2511ef32fac72e38a045634fa3409344eb4b481
tylerbreese <tylerbreese@arizona.edu>
2025-11-19 10:50:01 -0700

    try again, commmit latest data

--- bubble/matlab/easy_plane.sh
+++ bubble/matlab/easy_plane.sh
@@ -2,28 +2,22 @@
 PID_A=$!
 ./batch_plane.sh plane/input2.in & 
 PID_B=$!
-wait  $PID_A $PID_B
-
 ./batch_plane.sh plane/input3.in &
 PID_C=$! 
 ./batch_plane.sh plane/input4.in &
 PID_D=$!
-wait  $PID_C $PID_D 
-
 ./batch_plane.sh plane/input5.in &
 PID_E=$!
-./batch_plane.sh plane/input6.in &
-PID_F=$!
-wait  $PID_E $PID_F 
+wait $PID_A $PID_B $PID_C $PID_D $PID_E 
 
-./batch_plane.sh plane/input7.in &
-PID_G=$! 
-./batch_plane.sh plane/input8.in &
-PID_H=$!
-wait $PID_G $PID_H 
-
-./batch_plane.sh plane/input9.in &
-PID_I=$! 
-./batch_plane.sh plane/input10.in &
-PID_J=$!
-wait $PID_I $PID_J 
+# ./batch_plane.sh plane/input6.in &
+# PID_F=$!
+# ./batch_plane.sh plane/input7.in &
+# PID_G=$! 
+# ./batch_plane.sh plane/input8.in &
+# PID_H=$!
+# ./batch_plane.sh plane/input9.in &
+# PID_I=$! 
+# ./batch_plane.sh plane/input10.in &
+# PID_J=$!
+# wait $PID_F $PID_G $PID_H $PID_I $PID_J 
